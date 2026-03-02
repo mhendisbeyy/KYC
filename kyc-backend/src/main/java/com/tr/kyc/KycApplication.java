b0a15c73f02a388d10ecb15f16fbb9c03ee9dfc6
Ali ATALAY <45068962+mhendisbeyy@users.noreply.github.com>
2026-03-02 03:40:09 +0300

    Update KycApplication.java

--- kyc-backend/src/main/java/com/tr/kyc/KycApplication.java
+++ kyc-backend/src/main/java/com/tr/kyc/KycApplication.java
@@ -2,15 +2,20 @@ package com.tr.kyc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
 
 @EnableScheduling
 @SpringBootApplication
-public class KycApplication {
+public class KycApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KycApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(KycApplication.class);
+	}
 }
